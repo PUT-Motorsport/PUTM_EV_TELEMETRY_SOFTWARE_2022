c62ab2884298254990b26e1c829b693feb5a457a
MChlebosz <mateusz.chlebosz@outlook.com>
2021-12-05 00:17:11 +0100

    Minor WS address changes

--- telemetry_app/.eslintrc.js
+++ telemetry_app/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
 		// add more generic rulesets here, such as:
 		//"eslint:recommended",
 		"plugin:vue/vue3-recommended",
-		// 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+		"eslint:recommended",
+		"@vue/prettier",
 		"prettier",
 	],
 	rules: {
--- telemetry_app/src/components/Connection.vue
+++ telemetry_app/src/components/Connection.vue
@@ -1,20 +1,26 @@
 <template>
   <div id="connection">
-    <input
-      id="serialport"
-      v-model="serialport"
-      type="text"
-      name="serialport"
-      placeholder="Enter Serial Port"
-    >
+    <select id="serialports" v-model="serialport" name="serialports">
+      <option
+        v-for="item in portList"
+        :key="item.id"
+        :value="item.val"
+        :selected="item.selected ? 'selected' : ''"
+      >
+        {{ item.val }} :
+        {{ item.full }}
+      </option>
+    </select>
     <input
       id="baud"
       v-model="baud"
+      :disabled="serialOn"
       type="number"
       name="baud"
       placeholder="enter baud"
-    >
+    />
     <button
+      :disabled="serialOn"
       @click="
         openPort(serialport, baud);
         serialOutput = [];
@@ -22,16 +28,12 @@
     >
       Open
     </button>
-    <!--   Here edit your path to arduino -->
-    <button @click="closePort(serialport, baud)">
+    {{ activePortName }}
+    <button :disabled="!serialOn" @click="closePort(serialport, baud)">
       Close
     </button>
     <ol id="outputList">
-      <li
-        v-for="item in serialOutput"
-        :key="item.id"
-        class="output"
-      >
+      <li v-for="item in serialOutput" :key="item.id" class="output">
         {{ item.values }}
       </li>
     </ol>
@@ -45,23 +47,22 @@ export default {
     return {
       serialOutput: [],
       connection: null,
+      activePortName: null,
+      serialOn: false,
+      portList: [],
+      serialport: null,
+      baud: null,
     };
   },
   created: function () {
     console.log("Starting connection to WebSocket Server");
-    this.connection = new WebSocket("ws://localhost:8989/ws");
+    this.connection = new WebSocket("ws://25.99.238.105:8989/ws");
     this.connection.onmessage = (event) => {
-      let jsonInput = JSON.parse(event.data);
-      console.log(jsonInput);
-      if (jsonInput[0] !== undefined) {
-        if (typeof jsonInput["D"] !== "undefined") {
-          jsonInput = jsonInput["D"].replace(" ", "");
-          if (jsonInput.length > 0) {
-            const vals = jsonInput.split("/").map((x) => Number(x));
-            const newitem = { id: Date.now(), values: vals };
-            this.serialOutput.push(newitem);
-          }
-        }
+      try {
+        let jsonInput = JSON.parse(event.data.toString());
+        this.handleJSON(jsonInput);
+      } catch (e) {
+        return false;
       }
     };
 
@@ -71,8 +72,9 @@ export default {
       this.sendMessage("list");
     };
   },
-  mounted: () => {
-    this.sendMessage("list");
+  mounted: function () {
+    this.serialport = this.getCookie("lastPort");
+    this.baud = this.getCookie("lastBaud");
   },
   methods: {
     sendMessage: function (message) {
@@ -81,9 +83,51 @@ export default {
     },
     openPort(port, baud) {
       this.sendMessage("open " + port + " " + baud);
+      document.cookie = "lastPort = " + port;
+      document.cookie = "lastBaud = " + baud;
+      this.serialOn = true;
     },
     closePort(port) {
       this.sendMessage("close " + port);
+      this.serialOn = false;
+    },
+    getCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(";").shift();
+    },
+    handleSerialList(jsonInput) {
+      this.portList = jsonInput.map((x, index) => ({
+        id: index,
+        val: x.Name,
+        selected: x.Name == this.getCookie("lastPort"),
+        full: x.Friendly,
+      }));
+      const activePort = jsonInput.find((element) => element.IsOpen == true);
+      if (activePort !== undefined) {
+        const activePortName = activePort.Name;
+        this.activeportName = activePortName;
+        const currentBaud = jsonInput.find(
+          (element) => element.Name == activePortName
+        )["Baud"];
+        document.cookie = "lastBaud=" + currentBaud;
+        this.baud = currentBaud;
+        this.serialOn = true;
+      }
+    },
+    handleJSON(jsonInput) {
+      if (typeof jsonInput["D"] != "undefined") {
+        jsonInput = jsonInput["D"].replace(" ", "");
+        if (jsonInput.length > 0) {
+          const vals = jsonInput.split("/").map((x) => Number(x));
+          const newitem = { id: Date.now(), values: vals };
+          this.serialOutput.push(newitem);
+        }
+      }
+      if (typeof jsonInput["SerialPorts"] != "undefined") {
+        const listData = jsonInput["SerialPorts"];
+        this.handleSerialList(listData);
+      }
     },
   },
 };
